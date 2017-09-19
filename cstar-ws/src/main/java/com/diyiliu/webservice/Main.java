1680b9c9b7f1da9d85c900da1987c76e75dff88b
DIYILIU <572772828@qq.com>
2017-09-19 08:56:45 +0800

    指令下发，更新tstar-common 包

---
+++ cstar-ws/src/main/java/com/diyiliu/webservice/Main.java
@@ -0,0 +1,16 @@
+package com.diyiliu.webservice;
+
+import com.diyiliu.common.util.SpringUtil;
+
+/**
+ * Description: Main
+ * Author: DIYILIU
+ * Update: 2017-09-18 16:33
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        SpringUtil.init("config/applicationContext.xml");
+    }
+}
